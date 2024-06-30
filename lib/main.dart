c2a19c456072e82f3dcae47d2b3fdd1b54ef3143
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-06-29 23:42:55 -0500

    feat: Add Text

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,30 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             CircleAvatar(
               radius: 56,
               backgroundColor: Colors.white24,
               backgroundImage: NetworkImage(
                   'https://images.pexels.com/photos/598917/pexels-photo-598917.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+            ),
+            Text(
+              'Marcos Medrano',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              'Flutter Delevoper',
+              style: TextStyle(
+                color: Colors.white54,
+                fontSize: 16,
+                letterSpacing: 2,
+                fontWeight: FontWeight.w300,
+              ),
             )
           ],
         ),
