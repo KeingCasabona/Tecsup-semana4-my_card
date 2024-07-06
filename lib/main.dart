8b8f96563a385de2ae0f3400da29c9f9c2ee64ae
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-07-06 00:07:26 -0500

    feat: Add font, card, listTile, icons

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
-        backgroundColor: Colors.black12,
-        appBar: AppBar(
-          backgroundColor: Colors.black12,
-          title: const Text('Material App Bar',
-              style: TextStyle(color: Colors.white)),
-        ),
+        backgroundColor: Color.fromARGB(255, 10, 55, 93),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           //crossAxisAlignment: CrossAxisAlignment.center,
@@ -25,14 +21,17 @@ class MyApp extends StatelessWidget {
               backgroundColor: Colors.white24,
               backgroundImage: AssetImage('assets/images/person.jpeg'),
             ),
+            SizedBox(height: 16),
             Text(
               'Marcos Medrano',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 24,
                 fontWeight: FontWeight.w700,
+                fontFamily: "Roboto",
               ),
             ),
+            SizedBox(height: 4),
             Text(
               'Flutter Delevoper',
               style: TextStyle(
@@ -40,7 +39,60 @@ class MyApp extends StatelessWidget {
                 fontSize: 16,
                 letterSpacing: 2,
                 fontWeight: FontWeight.w300,
+                fontFamily: "Roboto",
               ),
+            ),
+            SizedBox(
+              width: 120,
+              child: Divider(
+                color: Colors.white,
+                thickness: 0.5,
+              ),
+            ),
+            Card(
+              elevation: 4,
+              color: Colors.white,
+              shadowColor: const Color.fromARGB(145, 255, 255, 255),
+              margin: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+              child: ListTile(
+                leading: Icon(Icons.phone, color: Colors.green),
+                title: Text("+51 234 567 345"),
+                subtitle: Text("Teléfono"),
+                trailing: Icon(Icons.check_circle_outline, color: Colors.blue),
+              ),
+            ),
+            Card(
+              elevation: 4,
+              color: Colors.white,
+              shadowColor: const Color.fromARGB(145, 255, 255, 255),
+              margin: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+              child: ListTile(
+                leading: Icon(Icons.email, color: Colors.green),
+                title: Text("keing.l344@gmail.com"),
+                subtitle: Text("Correo electrónico"),
+                trailing: Icon(Icons.check_circle_outline, color: Colors.blue),
+              ),
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  'assets/icons/facebook2.png',
+                  height: 60,
+                  color: Colors.white,
+                ),
+                Image.asset(
+                  'assets/icons/gorjeo.png',
+                  height: 60,
+                  color: Colors.white,
+                ),
+                Image.asset(
+                  'assets/icons/instagram.png',
+                  height: 60,
+                  color: Colors.white,
+                ),
+              ],
             )
           ],
         ),
